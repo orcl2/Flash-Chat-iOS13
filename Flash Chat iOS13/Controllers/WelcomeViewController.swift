84e953a075399127e3312dda2984191f48608074
William Daniel <wdarchenemy@gmail.com>
2022-04-22 11:07:37 -0300

    wip: adding CLTypingLabel pod

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,16 +7,30 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        titleLabel.text = "⚡️FlashChat"
     }
     
-
+    //Other way to do the typing effect
+//    override func viewDidAppear(_ animated: Bool) {
+//
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//
+//        for char in "⚡️FlashChat" {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+//                self.titleLabel.text?.append(char)
+//            }
+//
+//            charIndex += 1
+//        }
+//    }
 }
